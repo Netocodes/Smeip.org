d35a1af0cd0c3ea3a226b3d30ffa3790b1729753
Netochukwu Codes <161637474+Netocodes@users.noreply.github.com>
2024-08-19 19:57:28 +0100

    updated ui

--- src/script.js
+++ src/script.js
@@ -1,6 +1,32 @@
+alert('after veiwing pls rest starkie so i can work pacefully, ps the text here is of no use its random and not well thought of.. NetoCodes')
+
 // Force light mode
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   document.documentElement.classList.add('force-light-mode');
   alert("turn off dark mode on your mobile")
   window.location.href = 'https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.youtube.com/watch%3Fv%3DUxs_GRhKnR8&ved=2ahUKEwjNhPOclfyHAxXjQUEAHdTEMVIQtwJ6BAgNEAI&usg=AOvVaw0BV7FF8XrQXqp-E5IaX3cY'
 }
+
+
+
+  const fixedNav = document.getElementById("navigation");
+
+document.addEventListener('scroll', () => {
+    if(window.scrollY > 56){
+    fixedNav.classList.add("fixed"),
+    fixedNav.classList.add("top-0"),
+    fixedNav.classList.add("md:bg-black/75"),
+    fixedNav.classList.add("bg-black"),
+    fixedNav.classList.add("md:py-0"),
+    fixedNav.classList.add("shadow-lg")
+   
+}
+else{
+    fixedNav.classList.remove("fixed")
+    fixedNav.classList.remove("top-0"),
+    fixedNav.classList.remove("bg-black"),
+    fixedNav.classList.remove("md:py-0"),
+    fixedNav.classList.remove("shadow-lg")
+}
+});
+
